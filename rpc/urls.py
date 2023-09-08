d50cb0f6b77fd075d8fe49e835bfd13d70fe330a
Jennifer Richards <jennifer@staff.ietf.org>
2023-09-08 13:00:16 -0300

    feat: Add OIDC login

--- rpc/urls.py
+++ rpc/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from core import views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("oidc/", include("mozilla_django_oidc.urls")),
     path("", views.index),
 ]

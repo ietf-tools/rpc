3a4ac129cc4e9e8efc5b12e9260532219315aa8a
Jennifer Richards <jennifer@staff.ietf.org>
2023-09-08 12:57:30 -0300

    feat: Add minimal index view

--- rpc/urls.py
+++ rpc/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core import views
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.index),
 ]

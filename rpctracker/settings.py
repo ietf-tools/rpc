e3aad8f89933952847c069ef32eaf0a93d338811
Jennifer Richards <jennifer@staff.ietf.org>
2023-09-13 17:17:09 -0300

    refactor: Move django proj to rpctracker

--- rpctracker/settings.py
+++ rpctracker/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "rpc.urls"
+ROOT_URLCONF = "rpctracker.urls"
 
 TEMPLATES = [
     {
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "rpc.wsgi.application"
+WSGI_APPLICATION = "rpctracker.wsgi.application"
 
 
 # Database

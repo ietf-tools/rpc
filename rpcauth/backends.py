4fce1546ece81b93fe2265e462a5facec2cf1830
Jennifer Richards <jennifer@staff.ietf.org>
2023-09-14 11:23:51 -0300

    feat: Cache datatracker's plain_name

--- rpcauth/backends.py
+++ rpcauth/backends.py
@@ -9,13 +9,21 @@ from rpc.models import DatatrackerPerson
 
 
 class RpcOIDCAuthBackend(OIDCAuthenticationBackend):
-    """Customized OIDC auth backend"""
+    """Customized OIDC auth backend
+
+    Assumes the datatracker makes the following claims:
+      sub - pk of the Person who was authenticated, as a string
+      name - plain_name of the Person identified by sub
+      roles - list of 2-tuples corresponding to Person's active roles
+    """
 
     def create_user(self, claims):
+        """Create a User following a successful auth"""
         subject_id = claims["sub"]
         # A DatatrackerPerson may exist without a user so use get_or_create()
         datatracker_person, _ = DatatrackerPerson.objects.get_or_create(
-            subject_id=subject_id
+            subject_id=subject_id,
+            plain_name=claims.get("name", ""),
         )
         try:
             # Rely on the OneToOne field to avoid creating duplicate Users for a DatatrackerPerson
@@ -28,13 +36,27 @@ class RpcOIDCAuthBackend(OIDCAuthenticationBackend):
             raise SuspiciousOperation(f"User already exists for datatracker person pk={subject_id}")
         return new_user
 
+    def update_user(self, user, claims):
+        """Update a User following a successful auth"""
+        # Update the name if we got it
+        plain_name = claims.get("name", None)
+        if plain_name is not None and plain_name != user.datatracker_person.plain_name:
+            user.datatracker_person.plain_name = plain_name
+            user.datatracker_person.save()
+
     def filter_users_by_claims(self, claims):
+        """Return list or queryset of users who satisfy claims
+
+        If the set does not have exactly one member, auth will be rejected.
+        """
+        # Only consider users who have a datatracker_person!
         return self.UserModel.objects.filter(
             datatracker_person__subject_id__isnull=False,
             datatracker_person__subject_id=claims["sub"],  # claim guaranteed to exist
         )
 
     def verify_claims(self, claims):
+        """Verify that claims are sufficient to allow auth"""
         required_claims = {"sub", "roles"}
         if required_claims.intersection(claims.keys()) != required_claims:
             return False
--- rpctracker/settings.py
+++ rpctracker/settings.py
@@ -102,7 +102,7 @@ AUTHENTICATION_BACKENDS = (
 OIDC_RP_CLIENT_ID = os.environ["OIDC_RP_CLIENT_ID"]
 OIDC_RP_CLIENT_SECRET = os.environ["OIDC_RP_CLIENT_SECRET"]
 OIDC_RP_SIGN_ALGO = "RS256"
-OIDC_RP_SCOPES = "openid email profile roles"
+OIDC_RP_SCOPES = "openid profile roles"
 
 OIDC_OP_JWKS_ENDPOINT = "http://host.docker.internal:8000/api/openid/jwks/"
 OIDC_OP_AUTHORIZATION_ENDPOINT = (

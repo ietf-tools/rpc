b0384b86cb5142302cbca0f922efbf574a2cb1d7
Jennifer Richards <jennifer@staff.ietf.org>
2023-09-16 09:43:29 -0300

    chore: Add RpcPerson and closely related models

--- rpc/models.py
+++ rpc/models.py
@@ -24,3 +24,44 @@ class DatatrackerPerson(models.Model):
         blank=True,
         help_text="Datatracker's plain name for this Person",
     )
+
+    def __str__(self):
+        return self.plain_name or f"Person {self.subject_id}"
+
+
+class RpcPerson(models.Model):
+    datatracker_person = models.ForeignKey(
+        "DatatrackerPerson",
+        on_delete=models.PROTECT
+    )
+    can_hold_role = models.ManyToManyField("RpcRole")
+    capable_of = models.ManyToManyField("Capability")
+    hours_per_week = models.PositiveSmallIntegerField(default=40)
+    manager = models.ForeignKey(
+        "RpcPerson",
+        null=True,
+        on_delete=models.PROTECT,
+        limit_choices_to={"can_hold_role__slug": "manager"},
+        related_name="managed_people",
+    )
+
+    def __str__(self):
+        return str(self.datatracker_person)
+
+
+class RpcRole(models.Model):
+    slug = models.CharField(max_length=32, primary_key=True)
+    name = models.CharField(max_length=255)
+    desc = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Capability(models.Model):
+    slug = models.CharField(max_length=32, primary_key=True)
+    name = models.CharField(max_length=255)
+    desc = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name

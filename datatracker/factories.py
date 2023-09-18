b1cbb18c4fb78d9ecb581be2db3727698b74099b
Jennifer Richards <jennifer@staff.ietf.org>
2023-09-18 15:21:52 -0300

    test: Add factories

---
+++ datatracker/factories.py
@@ -0,0 +1,24 @@
+# Copyright The IETF Trust 2023, All Rights Reserved
+# -*- coding: utf-8 -*-
+
+import factory
+
+from .models import (
+    DatatrackerPerson,
+    Document,
+)
+
+
+class DatatrackerPersonFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = DatatrackerPerson
+
+    subject_id = factory.Faker("pystr_format", string_format="######")
+    plain_name = factory.Faker("name")
+
+
+class DocumentFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Document
+
+    datatracker_id = factory.Faker("pyint", min_value=1, max_value=10_000_000)
---
+++ rpc/factories.py
@@ -0,0 +1,208 @@
+# Copyright The IETF Trust 2023, All Rights Reserved
+# -*- coding: utf-8 -*-
+
+import factory
+
+from django.db.models import Max
+
+from .models import (
+    ActionHolder,
+    Assignment,
+    Capability,
+    Cluster,
+    DispositionName,
+    FinalApproval,
+    RfcAuthor,
+    RfcToBe,
+    RpcAuthorComment,
+    RpcPerson,
+    RpcRole,
+    SourceFormatName,
+    StdLevelName,
+    StreamName,
+    TlpBoilerplateChoiceName,
+    UnusableRfcNumber,
+)
+
+
+class RpcPersonFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = RpcPerson
+
+    datatracker_person = factory.SubFactory(
+        "datatracker.factories.DatatrackerPersonFactory"
+    )
+
+    @factory.post_generation
+    def can_hold_role(self, create, extracted, **kwargs):
+        if not create or not extracted:
+            return
+        for item in extracted:
+            if isinstance(item, str):
+                self.can_hold_role.add(RpcRoleFactory(slug=item, **kwargs))
+            elif isinstance(item, RpcRole):
+                self.can_hold_role.add(item)
+            else:
+                raise Exception(f"Cannot add {item} to can_hold_role")
+
+    @factory.post_generation
+    def capable_of(self, create, extracted, **kwargs):
+        if not create or not extracted:
+            return
+        for item in extracted:
+            if isinstance(item, str):
+                self.capable_of.add(CapabilityFactory(slug=item, **kwargs))
+            elif isinstance(item, Capability):
+                self.capable_of.add(item)
+            else:
+                raise Exception(f"Cannot add {item} to can_hold_role")
+
+
+class RpcRoleFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = RpcRole
+        django_get_or_create = ("slug",)
+
+    slug = factory.Faker("word")
+    name = factory.Faker("sentence")
+    desc = factory.Faker("sentence")
+
+
+class CapabilityFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Capability
+        django_get_or_create = ("slug",)
+
+    slug = factory.Faker("word")
+    name = factory.Faker("sentence")
+    desc = factory.Faker("sentence")
+
+
+class DispositionNameFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = DispositionName
+        django_get_or_create = ("slug",)
+
+    slug = factory.Faker("word")
+    name = factory.Faker("sentence")
+    desc = factory.Faker("sentence")
+
+
+class RfcToBeFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = RfcToBe
+
+    disposition = factory.SubFactory(DispositionNameFactory, slug="in_progress")
+    draft = factory.SubFactory("datatracker.factories.DocumentFactory")
+    submitted_format = factory.SubFactory(
+        "rpc.factories.SourceFormatNameFactory", slug="xml-v3"
+    )
+    submitted_std_level = factory.SubFactory(
+        "rpc.factories.StdLevelNameFactory", slug="ps"
+    )
+    submitted_boilerplate = factory.SubFactory(
+        "rpc.factories.TlpBoilerplateChoiceNameFactory", slug="trust200902"
+    )
+    submitted_stream = factory.SubFactory(
+        "rpc.factories.StreamNameFactory", slug="ietf"
+    )
+    intended_std_level = factory.LazyAttribute(lambda o: o.submitted_std_level)
+    intended_boilerplate = factory.LazyAttribute(lambda o: o.submitted_boilerplate)
+    intended_stream = factory.LazyAttribute(lambda o: o.submitted_stream)
+
+
+class AprilFirstRfcToBeFactory(RfcToBeFactory):
+    is_april_first_rfc = True
+    draft = None
+
+
+class ActionHolderFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ActionHolder
+
+    datatracker_person = factory.SubFactory(
+        "datatracker.factories.DatatrackerPersonFactory"
+    )
+    comment = factory.Faker("sentence")
+
+
+class RpcAuthorCommentFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = RpcAuthorComment
+
+    datatracker_person = factory.SubFactory(
+        "datatracker.factories.DatatrackerPersonFactory"
+    )
+    comment = factory.Faker("sentence")
+    by = factory.SubFactory("datatracker.factories.DatatrackerPersonFactory")
+
+
+class ClusterFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Cluster
+
+    number = factory.LazyFunction(
+        lambda: 1 + (Cluster.objects.aggregate(Max("number"))["number__max"] or 0)
+    )
+
+
+class UnusableRfcNumberFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = UnusableRfcNumber
+
+    number = factory.LazyFunction(
+        lambda: 1
+        + (UnusableRfcNumber.objects.aggregate(Max("number"))["number__max"] or 0)
+    )
+    comment = factory.Faker("sentence")
+
+
+class AssignmentFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Assignment
+
+    rfc_to_be = factory.SubFactory(RfcToBeFactory)
+    role = factory.SubFactory(RpcRoleFactory)
+    person = factory.SubFactory(RpcPersonFactory)
+
+
+class RfcAuthorFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = RfcAuthor
+
+    datatracker_person = factory.SubFactory(
+        "datatracker.factories.DatatrackerPersonFactory"
+    )
+    rfc_to_be = factory.SubFactory(RfcToBeFactory)
+
+
+class FinalApprovalFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = FinalApproval
+
+    rfc_to_be = factory.SubFactory(RfcToBeFactory)
+    approver = factory.SubFactory("datatracker.factories.DatatrackerPersonFactory")
+
+
+class SourceFormatNameFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = SourceFormatName
+        django_get_or_create = ("slug",)
+
+
+class StdLevelNameFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = StdLevelName
+        django_get_or_create = ("slug",)
+
+
+class TlpBoilerplateChoiceNameFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = TlpBoilerplateChoiceName
+        django_get_or_create = ("slug",)
+
+
+class StreamNameFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = StreamName
+        django_get_or_create = ("slug",)

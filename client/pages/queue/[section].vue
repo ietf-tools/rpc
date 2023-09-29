a80639d4ec47f515909c85744c3e5233296dc82d
Jennifer Richards <jennifer@staff.ietf.org>
2023-09-28 21:33:14 -0300

    chore: Roll separate queue pages into new structure

--- client/pages/queue/[section].vue
+++ client/pages/queue/[section].vue
@@ -189,12 +189,27 @@ const currentTab = computed(() => {
 
 // INIT
 
-const { data: documents, pending, refresh } = await useFetch('/api/rpc/submissions/', {
+const {data: documents, pending, refresh} = await useFetch(
+  () => currentTab.value === 'submissions' ? '/api/rpc/submissions/' : '/api/rpc/queue/', {
   baseURL: '/',
   server: false,
   lazy: true,
   data: () => ([]),
-  transform: (resp) => resp?.submitted ?? [],
+  transform: (resp) => {
+    // choose documents to list
+    if (currentTab.value === 'submissions') {
+      return resp?.submitted ?? []
+    }
+    const docs = resp?.queue ?? []
+    switch (currentTab.value) {
+      case 'pending':
+        return docs.filter(d => d.assignments.length === 0)
+      case 'inprocess':
+        return docs.filter(d => d.assignments.length > 0)
+      default:
+        return []
+    }
+  },
   onRequestError ({ error }) {
     state.notifDialogMessage = error
     state.notifDialogShown = true

a0aa2f2d91a55642d271b0f9fcf6d6813f3a3871
Jennifer Richards <jennifer@staff.ietf.org>
2023-10-03 23:04:31 -0300

    refactor: Extract TitleBlock cpt from team page

---
+++ client/components/TitleBlock.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="sm:flex sm:items-center">
+    <div class="sm:flex-auto">
+      <h1 class="text-base font-semibold leading-6 text-gray-900 dark:text-neutral-200">
+        {{ props.title }}
+      </h1>
+      <p v-if="props.summary" class="mt-2 text-sm text-gray-700 dark:text-neutral-400">
+        {{ props.summary }}
+      </p>
+    </div>
+    <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex">
+      <slot name="buttons"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: { type: String, required: true },
+  summary: { type: String }
+})
+</script>
--- client/pages/team/index.vue
+++ client/pages/team/index.vue
@@ -1,25 +1,21 @@
 <template>
   <div class="px-4 py-10 sm:px-6 lg:px-8 lg:py-6">
-    <div class="sm:flex sm:items-center">
-      <div class="sm:flex-auto">
-        <h1 class="text-base font-semibold leading-6 text-gray-900 dark:text-neutral-200">
-          Manage Team Members
-        </h1>
-        <p class="mt-2 text-sm text-gray-700 dark:text-neutral-400">
-          A list of all the users having access to this tool.
-        </p>
-      </div>
-      <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex">
+    <TitleBlock title="Manage Team Members"
+                summary="A list of all the users having access to this tool.">
+      <template v-slot:buttons>
         <button type="button" class="btn-secondary mr-3">
           <span class="sr-only">Refresh</span>
-          <Icon name="solar:refresh-line-duotone" size="1.5em" @click="refresh" :class="[pending ? 'animate-spin text-orange-600' : 'text-gray-500 dark:text-neutral-300']" aria-hidden="true" />
+          <Icon name="solar:refresh-line-duotone" size="1.5em" @click="refresh"
+                :class="[pending ? 'animate-spin text-orange-600' : 'text-gray-500 dark:text-neutral-300']"
+                aria-hidden="true"/>
         </button>
-        <button type="button" @click="state.createDialogShown = true" class="flex items-center rounded-md bg-violet-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-          <Icon name="uil:plus" class="-ml-1 h-5 w-5 mr-2" aria-hidden="true" />
+        <button type="button" @click="state.createDialogShown = true"
+                class="flex items-center rounded-md bg-violet-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          <Icon name="uil:plus" class="-ml-1 h-5 w-5 mr-2" aria-hidden="true"/>
           New Team Member
         </button>
-      </div>
-    </div>
+      </template>
+    </TitleBlock>
     <div class="mt-8 flow-root">
       <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">

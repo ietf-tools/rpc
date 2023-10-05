34f0c85391d67cd2f373fcf73a0062aacd350aaa
Jennifer Richards <jennifer@staff.ietf.org>
2023-10-04 23:04:15 -0300

    refactor: Update User model
    Not squashing the migration changes for now

--- rpcauth/models.py
+++ rpcauth/models.py
@@ -8,9 +8,16 @@ from django.db import models
 class User(AbstractUser):
     """RPC tool user class"""
 
+    name = models.CharField(
+        max_length=255,
+        help_text="User's name",
+    )
+
     datatracker_subject_id = models.CharField(
         max_length=255,  # per OpenID Core 1.0, 255 ASCII chars is the limit
         null=True,
         unique=True,
         help_text="Datatracker's subject ID for this User",
     )
+
+    avatar = models.URLField(blank=True)

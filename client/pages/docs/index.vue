ec2d49748cb220e170cd80a7a9035f9f4d8eb55b
Jennifer Richards <jennifer@staff.ietf.org>
2023-10-06 23:53:28 -0300

    chore: Link to unassigned page
    Just so there is one...

--- client/pages/docs/index.vue
+++ client/pages/docs/index.vue
@@ -1,3 +1,9 @@
 <template>
-  <span>Beep boop</span>
+  <TitleBlock title="Documents" summary="Beep boop"/>
+  <div class="mt-8 flow-root">
+    <a href="/docs/unassigned"
+       class="max-w-l items-center rounded-md bg-violet-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+      Manage Unassigned
+    </a>
+  </div>
 </template>

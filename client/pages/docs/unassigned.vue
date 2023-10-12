bb8bc635debd2c00ae615b3ce7ed32f4189d2b29
Jennifer Richards <jennifer@staff.ietf.org>
2023-10-12 11:14:50 -0300

    refactor: Clean up unassigned.vue

--- client/pages/docs/unassigned.vue
+++ client/pages/docs/unassigned.vue
@@ -3,8 +3,8 @@
     <template #right>
       <button type="button" class="btn-secondary mr-3">
         <span class="sr-only">Refresh</span>
-        <Icon name="solar:refresh-line-duotone" size="1.5em" @click="refresh"
-              :class="[(queuePending || peoplePending) ? 'animate-spin text-orange-600' : 'text-gray-500 dark:text-neutral-300']"
+        <Icon name="solar:refresh-line-duotone" size="1.5em"
+              class="text-gray-500 dark:text-neutral-300"
               aria-hidden="true"/>
       </button>
     </template>
@@ -12,72 +12,33 @@
 
   <div class="mt-8 flow-root">
     <h2>Documents for assignment</h2>
-    <!--    <div v-for="(doc, index) of documents" :key="doc.id"-->
-    <!--         class="w-full max-w-5xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">-->
-    <!--      <div class="space-y-1">-->
-    <!--        <h5 class="text-xl font-medium text-gray-900 dark:text-white">{{ doc.name }}</h5>-->
-    <!--        <div>{{ doc.title }}</div>-->
-    <!--        <div>Labels here</div>-->
-    <!--        <div>Pages: {{ doc.pages }}</div>-->
-    <!--        <div>View Notes (with count)</div>-->
-
-    <!--        <label :for="'editor' + doc.id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">-->
-    <!--          Assign editor-->
-    <!--        </label>-->
-    <!--        <select :id="'editor' + doc.id"-->
-    <!--                v-model="state.newAssignments[index].personId"-->
-    <!--                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">-->
-    <!--          <option :value="null">Leave unassigned</option>-->
-    <!--          <option v-for="editor of editors" :key="editor.id" :value="editor.id">-->
-    <!--            {{ editor.name }}-->
-    <!--          </option>-->
-    <!--        </select>-->
-    <!--      </div>-->
-    <!--    </div>-->
-    <!--    <Button @click="saveAssignments(state.newAssignments)">Save Changes</Button>-->
-    <!--    <Button @click="state.newAssignments = buildAssignments(documents, [])">Reset</Button>-->
-    <!--  </div>-->
-    <DocumentCards :documents="documents"/>
-    <EditorDeck :editors="editors"/>
+    <DocumentCards :documents="documents"
+                   @assignEditorToDocument="(dId, edId) => saveAssignments([{rfcToBeId: dId, personId: edId}])"/>
+    <EditorPalette :editors="people"/>
   </div>
 </template>
 
 <script setup>
 const csrf = useCookie('csrftoken', { sameSite: 'strict' })
 
-const state = reactive({
-  // newAssignments is an array of { rfcToBeId, personId } in the same order as the documents list
-  newAssignments: []
-})
-
 // COMPUTED
 
-const editors = computed(() => {
-  if (!people.value) {
-    return []
-  }
-  return people.value.map((person) => ({
-    id: person.id,
-    name: person.name
-  }))
-})
+const cookedAssignments = computed(() => assignments.value?.map(a => ({
+  ...a,
+  // person is a Person id - replace it with person details
+  person: people.value?.find(ed => ed.id === a.person)
+})))
 
-const documents = computed(() => {
-  if (!queue.value) {
-    return []
-  }
-  return queue.value.filter((doc) => doc.assignments.length === 0).map((doc) => ({
-    id: doc.id,
-    name: doc.name,
-    title: doc.title || 'unknown title',
-    pages: doc.pages || 'unknown'
-  }))
-})
+const documents = computed(() => rfcsToBe.value?.map((rtb) => ({
+  ...rtb,
+  // augment with assignments
+  assignments: cookedAssignments.value?.filter(a => a.rfc_to_be === rtb.id)
+})))
 
 // METHODS
 
 async function saveAssignments (assignments) {
-  state.newAssignments.forEach(({ rfcToBeId, personId }) => {
+  assignments.forEach(({ rfcToBeId, personId }) => {
     $fetch('/api/rpc/assignments/', {
       body: {
         rfc_to_be: rfcToBeId,
@@ -90,42 +51,14 @@ async function saveAssignments (assignments) {
   })
 }
 
-// Build assignments for a doc list, preserving old any old assignments
-function buildAssignments (newDocuments, oldAssignments) {
-  const oldAssignmentMap = Object.fromEntries(oldAssignments.map(oa => [oa.rfcToBeId, oa.personId]))
-  return newDocuments.map((doc) => ({
-    rfcToBeId: doc.id,
-    personId: oldAssignmentMap[doc.id] ?? null
-  }))
-}
-
-async function refresh () {
-  await peopleRefresh()
-  await queueRefresh()
-}
-
 // DATA RETRIEVAL
 
-const { data: people, pending: peoplePending, refresh: peopleRefresh } = await useFetch('/api/rpc/rpc_person/', {
-  baseURL: '/',
-  server: false
-})
-
-const { data: queue, pending: queuePending, refresh: queueRefresh } = await useFetch('/api/rpc/queue/', {
-  baseURL: '/',
-  server: false,
-  transform: (resp) => (resp?.queue || [])
-})
-
-onMounted(() => {
-  watch(documents, (newDocuments) => {
-    state.newAssignments = buildAssignments(newDocuments, state.newAssignments)
-  })
-})
+const { data: people } = await useFetch('/api/rpc/rpc_person/', { baseURL: '/', server: false })
+const { data: rfcsToBe } = await useFetch('/api/rpc/documents/', { baseURL: '/', server: false })
+const { data: assignments } = await useFetch('/api/rpc/assignments/', { baseURL: '/', server: false })
 
 /**
  * Todo
- * - use state instead of mutating computed values
  * - indicate changed assignments
  * - buttons to refresh/reset the view
  * - compute stats for editors (see details on wireframe)

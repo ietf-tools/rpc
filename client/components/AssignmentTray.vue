9c2f5628cf295c8fda4052128e40476f4716b6fc
Jennifer Richards <jennifer@staff.ietf.org>
2023-10-12 11:11:20 -0300

    feat: AssignmentTray cpt + sortablejs-vue3

---
+++ client/components/AssignmentTray.vue
@@ -0,0 +1,34 @@
+<template>
+  <Sortable
+    :list="props.assignments"
+    item-key="id"
+    class="container flex flex-wrap gap-2"
+    :options="sortableOptions"
+    @add="event => $emit('assignEditor', event.clone.dataset.editorId)"
+    >
+    <template #item="{element, index}">
+      <div class="draggable">
+        <AssignmentTrayItem :assignment="element"/>
+      </div>
+    </template>
+  </Sortable>
+</template>
+
+<script setup>
+import { Sortable } from 'sortablejs-vue3'
+
+const props = defineProps({
+  assignments: Array
+})
+
+defineEmits(['assignEditor'])
+
+const sortableOptions = {
+  group: {
+    name: 'assignments',
+    pull: false,
+    put: ['editors']
+  },
+  sort: false
+}
+</script>
---
+++ client/components/AssignmentTrayItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    :key="assignment.id"
+    class="bg-pink-600 rounded-md">
+    <dl class="m-2 p-2">
+      <dt class="sr-only">Editor</dt>
+      <dd class="text-sm text-white text-center">{{ assignment.initials }}</dd>
+      <dt class="sr-only">Role</dt>
+      <dd class="text-xs text-pink-100 text-center">{{ assignment.description }}</dd>
+    </dl>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  assignment: Object
+})
+
+const assignment = computed(() => ({
+  id: props.assignment.id,
+  initials: props.assignment.person?.name,
+  description: props.assignment.role
+}))
+</script>

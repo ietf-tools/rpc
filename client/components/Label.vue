c8c5660c252fabe8ef8090902e62cb94324b460d
Jennifer Richards <jennifer@staff.ietf.org>
2023-10-20 16:09:58 -0300

    feat: <Label> component

---
+++ client/components/Label.vue
@@ -0,0 +1,18 @@
+<template>
+  <Badge :color="label.color">
+    <Icon v-if="label.is_exception" class="mr-2" name="pajamas:warning"/>
+    {{ label.slug }}
+  </Badge>
+</template>
+
+<script setup>
+defineProps({
+  /**
+   * Label to be rendered
+   */
+  label: {
+    type: Object,
+    required: true
+  }
+})
+</script>

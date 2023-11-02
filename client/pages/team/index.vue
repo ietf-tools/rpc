eaef3293652e2de76465797f8c38faf60d7de372
Jennifer Richards <jennifer@staff.ietf.org>
2023-11-02 15:14:32 -0300

    feat: Hide "Add Team Member" from non-mgrs

--- client/pages/team/index.vue
+++ client/pages/team/index.vue
@@ -3,7 +3,9 @@
               summary="A list of all the users having access to this tool.">
     <template #right>
       <RefreshButton :pending="pending" @refresh="refresh" class="mr-3"/>
-      <button type="button" @click="newTeamMember"
+      <button v-if="userStore.isManager"
+              type="button"
+              @click="newTeamMember"
               class="flex items-center rounded-md bg-violet-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
         <Icon name="uil:plus" class="-ml-1 h-5 w-5 mr-2" aria-hidden="true"/>
         New Team Member
@@ -47,6 +49,8 @@ const { openOverlayModal } = inject('overlayModal')
 useHead({
   title: 'Manage Team Members'
 })
+
+const userStore = useUserStore()
 
 // METHODS
 

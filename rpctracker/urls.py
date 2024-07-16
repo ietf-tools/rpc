dee9c61afe62a5010c4a1153715ac6c33bb436dc
Jennifer Richards <jennifer@staff.ietf.org>
2024-07-16 15:51:36 -0300

    fix: give QueueViewSet a distinct basename

--- rpctracker/urls.py
+++ rpctracker/urls.py
@@ -48,7 +48,7 @@ router = routers.DefaultRouter()
 router.register(r'assignments', rpc_api.AssignmentViewSet)
 router.register(r'documents', rpc_api.RfcToBeViewSet)
 router.register(r'labels', rpc_api.LabelViewSet)
-router.register(r'queue', rpc_api.QueueViewSet)
+router.register(r'queue', rpc_api.QueueViewSet, basename="queue")
 router.register(r'rpc_roles', rpc_api.RpcRoleViewSet)
 
 urlpatterns = [

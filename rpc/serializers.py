7cfc084b97b77eec63377b4a4b2cfddc1524a8b2
Jennifer Richards <jennifer@staff.ietf.org>
2024-08-09 12:25:11 -0300

    style: black

--- rpc/serializers.py
+++ rpc/serializers.py
@@ -144,19 +144,29 @@ class RfcToBeSerializer(serializers.ModelSerializer):
         ]
 
     def get_name(self, rfc_to_be) -> str:
-        return rfc_to_be.draft.name if rfc_to_be.draft else "Some Apr 1 RFC" # TODO: reconcile when we teach the app to handle Apr 1 RFCs
+        return (
+            rfc_to_be.draft.name if rfc_to_be.draft else "Some Apr 1 RFC"
+        )  # TODO: reconcile when we teach the app to handle Apr 1 RFCs
 
     def get_rev(self, rfc_to_be) -> str:
-        return rfc_to_be.draft.rev  if rfc_to_be.draft else "none" # TODO: reconcile when we teach the app to handle Apr 1 RFCs
+        return (
+            rfc_to_be.draft.rev if rfc_to_be.draft else "none"
+        )  # TODO: reconcile when we teach the app to handle Apr 1 RFCs
 
     def get_title(self, rfc_to_be) -> str:
-        return rfc_to_be.draft.title if rfc_to_be.draft else "Some Apr 1 RFC" # TODO: reconcile when we teach the app to handle Apr 1 RFCs
+        return (
+            rfc_to_be.draft.title if rfc_to_be.draft else "Some Apr 1 RFC"
+        )  # TODO: reconcile when we teach the app to handle Apr 1 RFCs
 
     def get_stream(self, rfc_to_be) -> str:
-        return rfc_to_be.draft.stream if rfc_to_be.draft else "ISE" # TODO: reconcile when we teach the app to handle Apr 1 RFCs
+        return (
+            rfc_to_be.draft.stream if rfc_to_be.draft else "ISE"
+        )  # TODO: reconcile when we teach the app to handle Apr 1 RFCs
 
     def get_pages(self, rfc_to_be) -> int:
-        return rfc_to_be.draft.pages if rfc_to_be.draft else 0 # TODO: reconcile when we teach the app to handle Apr 1 RFCs
+        return (
+            rfc_to_be.draft.pages if rfc_to_be.draft else 0
+        )  # TODO: reconcile when we teach the app to handle Apr 1 RFCs
 
     def get_cluster(self, rfc_to_be) -> Optional[int]:
         return rfc_to_be.cluster.number if rfc_to_be.cluster else None

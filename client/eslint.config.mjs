468b4bffd1a7064d5670845e783cbb9b34d03030
Jennifer Richards <jennifer@staff.ietf.org>
2024-09-05 20:51:44 -0300

    chore: migrate to eslint.config.mjs

---
+++ client/eslint.config.mjs
@@ -0,0 +1,63 @@
+import vue from 'eslint-plugin-vue'
+import globals from 'globals'
+import parser from 'vue-eslint-parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
+
+export default [
+  ...compat.extends(
+    'plugin:nuxt/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/stylistic',
+  ), {
+    plugins: {
+      vue,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jquery,
+        ...globals.node,
+        ...vue.environments['setup-compiler-macros']['setup-compiler-macros'],
+        d3: true,
+      },
+
+      parser: parser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+
+    rules: {
+      'vue/script-setup-uses-vars': 'error',
+      'vue/multi-word-component-names': 'off',
+      'vue/max-attributes-per-line': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
+      'no-undef': 'off',
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  }, {
+    ignores: [
+      '.nuxt/',
+      '.output/',
+      'rpctracker_client/'
+    ]
+  }
+]

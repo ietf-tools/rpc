a279cea32544f797af6496adc64950bb1493e077
Jennifer Richards <jennifer@staff.ietf.org>
2024-09-26 16:32:31 -0300

    refactor: Card component

---
+++ client/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="overflow-hidden rounded-lg bg-white shadow">
+    <div v-if="$slots.header" class="px-4 py-5 sm:px-6">
+      <slot name="header"/>
+    </div>
+    <div class="px-4 py-5 sm:p-6">
+      <slot/>
+    </div>
+    <div v-if="$slots.footer" class="px-4 py-5 sm:px-6">
+      <slot name="footer"/>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>

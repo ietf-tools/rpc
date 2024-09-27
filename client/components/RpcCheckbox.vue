c01b2f8d2522be6c0227d4f84424915daa50b0ff
Jennifer Richards <jennifer@staff.ietf.org>
2024-09-27 13:18:09 -0300

    refactor: extract RpcCheckbox component

---
+++ client/components/RpcCheckbox.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="relative flex items-start">
+    <div class="flex h-6 items-center">
+      <input
+        v-bind="$attrs"
+        :id="id"
+        :aria-describedby="desc ? `${id}-description` : ''"
+        :name="name"
+        type="checkbox"
+        :class="[caution ? 'border-rose-300 dark:border-rose-500 text-rose-700 dark:text-rose-700 hover:border-rose-400 focus:ring-rose-600' : 'border-gray-300 dark:border-neutral-500 text-violet-600 dark:text-violet-500 hover:border-violet-400 dark:hover:border-violet-500 focus:ring-violet-600 dark:focus:ring-violet-500', 'h-4 w-4 bg-white dark:bg-neutral-900 rounded border-2']"
+      >
+    </div>
+    <div class="ml-3 text-sm leading-6">
+      <label
+        :for="name"
+        :class="[caution ? 'text-rose-800 dark:text-rose-400' : 'text-gray-900 dark:text-neutral-200', 'font-medium']">
+        {{ label }}
+      </label>
+      <p
+        v-if="desc"
+        :id="`${id}-description`"
+        :class="[caution ? 'text-rose-700 dark:text-rose-500' : 'text-gray-500 dark:text-neutral-400', 'text-xs']">
+        {{ desc }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+// Fallthrough attributes are applied to an internal element via v-bind="$attrs"
+defineOptions({ inheritAttrs: false })
+
+type Props = {
+  caution: boolean,
+  desc?: string,
+  id: string,
+  label: string,
+  name: string
+}
+
+defineProps<Props>()
+
+</script>

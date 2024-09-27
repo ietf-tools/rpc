53a64f8f3bed8f3cb5568335d87409303c5799e7
Jennifer Richards <jennifer@staff.ietf.org>
2024-09-27 16:05:47 -0300

    feat: enqueue page and DocComplexityCard (wip)

---
+++ client/components/DocComplexityCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <Card>
+    <template #header>
+      <h3 class="text-base font-semibold">Complexity Checks</h3>
+    </template>
+
+    <fieldset>
+      <RpcCheckbox
+        v-for="capability in capabilities"
+        :key="capability.slug"
+        :label="capability.name"
+        :desc="capability.desc"/>
+    </fieldset>
+  </Card>
+
+</template>
+
+<script setup lang="ts">
+
+import type { Capability, RfcToBe } from '~/rpctracker_client'
+
+type Props = {
+  capabilities: Capability[]
+  rfcToBe: RfcToBe
+}
+
+defineProps<Props>()
+
+</script>
---
+++ client/pages/docs/[id]/enqueue.vue
@@ -0,0 +1,36 @@
+<template>
+  <Card>
+    <template #header>
+      <TitleBlock
+        class="pb-3"
+        :title="`Prep for Queue: ${rfcToBe?.name || '&hellip;'}`"
+        summary="Ready the incoming document for the editing queue."/>
+    </template>
+
+    <div v-if="rfcToBe" class="space-y-12">
+      <div class="border-b border-gray-900/10 pb-12 flex">
+        <DocComplexityCard :capabilities="capabilities" :rfc-to-be="rfcToBe"/>
+      </div>
+    </div>
+  </Card>
+</template>
+
+<script setup lang="ts">
+
+import type { Capability, RfcToBe } from '~/rpctracker_client'
+
+const route = useRoute()
+const api = useApi()
+
+const { data: rfcToBe } = await useAsyncData<RfcToBe>(
+  'rfcToBe',
+  () => api.documentsRetrieve({ draftName: route.params.id.toString() }),
+  { server: false }
+)
+
+const { data: capabilities } = await useAsyncData<Capability[]>(
+  'capabilities',
+  () => api.capabilitiesList(),
+  { default: () => ([]), server: false }
+)
+</script>

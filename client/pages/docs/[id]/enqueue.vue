3f596742afffcd6961df34318f262fa9c0d045cd
Jennifer Richards <jennifer@staff.ietf.org>
2024-09-30 15:09:05 -0300

    feat: skeleton of enqueue page

--- client/pages/docs/[id]/enqueue.vue
+++ client/pages/docs/[id]/enqueue.vue
@@ -13,16 +13,70 @@
         <DocComplexityCard :capabilities="capabilities"/>
         <DocExceptionsCard :labels="labels"/>
       </div>
+      <Card>
+        <template #header>
+          <h3 class="text-base font-semibold">
+            Document Dependencies
+          </h3>
+        </template>
+        <DocumentTable
+          :columns="columns"
+          :data="relatedDocuments"
+          row-key="name"/>
+        <Button btn-type="default">Add Dependency</Button>
+      </Card>
+      <Card>
+        <template #header>
+          <h3 class="text-base font-semibold">
+            Comments
+          </h3>
+        </template>
+        <RpcTextarea/>
+      </Card>
     </div>
+    <template #footer>
+      <Button btn-type="default">Document has exceptions&mdash;escalate</Button>
+      <Button btn-type="default">Add to queue</Button>
+    </template>
   </Card>
 </template>
 
 <script setup lang="ts">
 
 import type { Capability, Label, RfcToBe } from '~/rpctracker_client'
+import type { Column } from '~/components/DocumentTableTypes'
 
 const route = useRoute()
 const api = useApi()
+
+const columns: Column[] = [
+  {
+    key: 'name',
+    label: 'Document',
+    field: 'name',
+    classes: 'text-sm font-medium'
+  },
+  {
+    key: 'relationship',
+    label: 'Relationship',
+    field: 'relationship',
+    classes: 'text-sm font-medium'
+  },
+  {
+    key: 'currentState',
+    label: 'Current State',
+    field: 'currentState',
+    classes: 'text-sm font-medium'
+  },
+]
+
+const relatedDocuments = [
+  {
+    name: 'draft-some-other-draft',
+    relationship: 'Normative Reference',
+    currentState: 'Active WG document'
+  },
+]
 
 const { data: rfcToBe } = await useAsyncData<RfcToBe>(
   'rfcToBe',

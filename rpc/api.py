be30d5053bfedf6325105a91bd00632cf7e7fc4f
Jennifer Richards <jennifer@staff.ietf.org>
2024-12-13 18:00:23 -0400

    chore: remove unused imports

--- rpc/api.py
+++ rpc/api.py
@@ -8,9 +8,7 @@ from rest_framework.decorators import (
     action,
     api_view,
     permission_classes,
-    authentication_classes,
 )
-from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import serializers
